7d30c3562437d425183f5a0675b0bf0fc92a6542
kybeen <rei980627@gmail.com>
2024-01-04 21:52:28 +0900

    [setting] 기본 뷰 세팅

--- RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/ViewController.swift
@@ -7,12 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
         
+        setupButton()
+    }
+    
+    func setupButton() {
+        let myButton = UIButton()
+        myButton.setTitle("버튼", for: .normal)
+        myButton.setTitleColor(.systemBlue, for: .normal)
+        
+        view.addSubview(myButton)
+        myButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 
 
