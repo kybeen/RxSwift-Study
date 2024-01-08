52ec3341149b12785bdf339c088b9d7f09b2e263
kybeen <rei980627@gmail.com>
2024-01-08 15:40:18 +0900

    [feat] RxSwift 샘플 코드 작성을 위한 뷰 구성

--- RxSwiftStudy/TestView.swift
+++ RxSwiftStudy/TestView.swift
@@ -11,11 +11,31 @@ import SnapKit
 
 final class TestView: UIView {
     
-    lazy var myButton: UIButton = {
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.color = .white
+        return indicator
+    }()
+    
+    lazy var timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0.0"
+        label.font = UIFont.systemFont(ofSize: 30)
+        return label
+    }()
+    
+    lazy var loadButton: UIButton = {
         let button = UIButton()
-        button.setTitle("버튼", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitle("LOAD", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
         return button
+    }()
+    
+    lazy var editView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .lightGray
+        return textView
     }()
     
     override init(frame: CGRect) {
@@ -28,12 +48,34 @@ final class TestView: UIView {
     }
     
     func setupViews() {
-        addSubview(myButton)
-        myButton.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        addSubview(timerLabel)
+        timerLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview().inset(15)
+        }
+        
+        addSubview(loadButton)
+        loadButton.snp.makeConstraints { make in
+            make.top.equalTo(timerLabel.snp.bottom).offset(15)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(15)
+            make.height.equalTo(50)
+        }
+        
+        addSubview(editView)
+        editView.snp.makeConstraints { make in
+            make.top.equalTo(loadButton.snp.bottom).offset(15)
+            make.horizontalEdges.equalToSuperview().inset(15)
+            make.bottom.equalTo(safeAreaLayoutGuide)
+        }
+        
+        addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints { make in
+            make.centerY.equalTo(loadButton)
+            make.right.equalTo(loadButton).offset(-20)
         }
     }
 }
+
 
 // MARK: - 프리뷰 canvas 세팅
 import SwiftUI
