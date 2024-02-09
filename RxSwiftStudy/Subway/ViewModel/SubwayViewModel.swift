33ee131e9c706108cdb13e6e1caab3e2020c3445
kybeen <rei980627@gmail.com>
2024-02-09 15:41:42 +0900

    Subway 뷰모델에 CSV → 호선&역 딕셔너리 생성 메서드 추가

--- RxSwiftStudy/Subway/ViewModel/SubwayViewModel.swift
+++ RxSwiftStudy/Subway/ViewModel/SubwayViewModel.swift
@@ -19,9 +19,17 @@ import RxRelay
 
 class SubwayViewModel {
     
+    lazy var subwayDict = [String: String]()     // [호선ID : 호선이름]
+    lazy var stationDict = [String: String]()    // [역ID : 역이름]
+    
     // MARK: - ArrivalInfo 배열 값이 변경될 때마다 받아오는 Observable
     lazy var arrivalInfoObservable = PublishRelay<[ArrivalInfo]>()
     
+    init() {
+        loadCSV()
+    }
+    
+    // MARK: - 실시간 도착 지하철 정보를 요청하는 메서드
     func checkArrival(stationName: String) {
         
         _ = APIManager.fetchRealtimeArrivalRx(stationName: stationName)
@@ -39,5 +47,30 @@ class SubwayViewModel {
             }
             .take(1)
             .bind(to: arrivalInfoObservable)
+    }
+    
+    // MARK: - CSV 파일을 불러와 역과 호선의 ID-이름 쌍에 대한 딕셔너리를 만들어주는 메서드
+    func loadCSV() {
+        do {
+            let path = Bundle.main.path(forResource: "Station_Info", ofType: "csv")!
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            
+            let dataEncoded = String(data: data, encoding: .utf8)!
+            
+            var dataArr = dataEncoded.components(separatedBy: "\n")
+            dataArr.removeFirst()
+            for row in dataArr {
+                let rowData = row.components(separatedBy: ",")[0..<4]
+                let subwayID = rowData[0]       // 호선ID
+                let stationID = rowData[1]      // 역ID
+                let stationName = rowData[2]    // 역이름
+                let subwayName = rowData[3]     // 호선이름
+                
+                subwayDict[subwayID] = subwayName
+                stationDict[stationID] = stationName
+            }
+        } catch {
+            print("Failed to load Station_Info,csv")
+        }
     }
 }
