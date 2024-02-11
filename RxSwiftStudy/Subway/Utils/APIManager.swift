e612a3bb690251d52bfe218e57fa9fe805a08956
kybeen <rei980627@gmail.com>
2024-02-12 02:36:51 +0900

    String → URL 변환 시 인코딩 후 변환하도록 수정

--- RxSwiftStudy/Subway/Utils/APIManager.swift
+++ RxSwiftStudy/Subway/Utils/APIManager.swift
@@ -18,9 +18,9 @@ class APIManager {
     
     // MARK: - API 호출 메서드
     static func fetchRealtimeArrival(stationName: String, onComplete: @escaping (Result<Data, Error>) -> Void) {
-        let ArrivalUrl = "http://swopenAPI.seoul.go.kr/api/subway/\(Bundle.main.SUBWAY_API_KEY)/json/realtimeStationArrival/\(REQ_START_IDX)/\(REQ_END_IDX)/\(stationName)"
-        
-        URLSession.shared.dataTask(with: URL(string: ArrivalUrl)!) { (data, response, error) in
+        let arrivalUrl = "http://swopenAPI.seoul.go.kr/api/subway/\(Bundle.main.SUBWAY_API_KEY)/json/realtimeStationArrival/\(REQ_START_IDX)/\(REQ_END_IDX)/\(stationName)"
+        let encodedURL = arrivalUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        URLSession.shared.dataTask(with: URL(string: encodedURL)!) { (data, response, error) in
             if let error = error {
                 onComplete(.failure(error))
             }
